df266ae9dc119cd3c41cd13a3068ff30c64bf914
凯凯大哥大 <74481186+KAI-MAC@users.noreply.github.com>
2020-12-26 02:22:51 +0800

    Add files via upload

---
+++ 特效作业-kk/城市电梯五彩缤纷/js/sctj.js
@@ -0,0 +1,20 @@
+document.writeln('<script type="text\/javascript" src="\/\/s4.cnzz.com\/stat.php?id=300636&web_id=300636"><\/scr'+'ipt>');
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?398913ed58c9e7dfe9695953fb7b6799";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
